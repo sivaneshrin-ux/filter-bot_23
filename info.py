3242505c6bb717008b3ea0b27fb2702462f47213
sivaneshrin-ux <sivaneshrin@gmail.com>
2025-10-02 17:06:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'media_search')
 API_ID = int(environ.get('API_ID', '24160099'))
 API_HASH = environ.get('API_HASH', '381515f2540b0bf817a68af8b352f5b7')
-BOT_TOKEN = environ.get('BOT_TOKEN', "8216877978:AAEtZbtRXyqTDHs1eIe0seLaiSm2IBVFcTE")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8216877bE")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
